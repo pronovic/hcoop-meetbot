a39107c67a7baf059bf3746c23c62ddcb5e9b941
Kenneth J. Pronovici <pronovic@ieee.org>
2021-03-16 14:45:14 +0000

    Stub out template rendering with Genshi

--- src/hcoopmeetbotlogic/writer.py
+++ src/hcoopmeetbotlogic/writer.py
@@ -4,13 +4,43 @@
 """
 Writes meeting log and minutes to disk.
 """
+import os
+from typing import Any, Dict, TextIO
+
+from genshi.template import MarkupTemplate, TemplateLoader
+
 from hcoopmeetbotlogic.config import Config
 
-from .location import Locations, derive_locations
+from .location import Location, Locations, derive_locations
 from .meeting import Meeting
+
+_TEMPLATES = os.path.join(os.path.dirname(__file__), "templates")
+_LOADER = TemplateLoader(search_path=_TEMPLATES, auto_reload=False)
+
+
+def _render_html(template: str, context: Dict[str, Any], out: TextIO) -> None:
+    """Render the named template to HTML, writing into the provided file."""
+    renderer = _LOADER.load(filename=template, cls=MarkupTemplate)  # type: MarkupTemplate
+    renderer.generate().render(method="html", doctype="html", out=out, **context)
+
+
+# TODO: remove unused-argument
+def _write_log(location: Location, meeting: Meeting) -> None:  # pylint: disable=unused-argument:
+    """Write the meeting log to disk."""
+    with open(location.path, "x") as out:
+        _render_html(template="log.html", context={}, out=out)
+
+
+# TODO: remove unused-argument
+def _write_minutes(location: Location, meeting: Meeting) -> None:  # pylint: disable=unused-argument:
+    """Write the meeting minutes to disk."""
+    with open(location.path, "x") as out:
+        _render_html(template="minutes.html", context={}, out=out)
 
 
 def write_meeting(config: Config, meeting: Meeting) -> Locations:
     """Write meeting files to disk, returning the file locations."""
-    # TODO: implement the real file save behavior
-    return derive_locations(config, meeting)
+    locations = derive_locations(config, meeting)
+    _write_log(location=locations.log, meeting=meeting)
+    _write_minutes(location=locations.minutes, meeting=meeting)
+    return locations
--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,18 +1,34 @@
 # -*- coding: utf-8 -*-
 # vim: set ft=python ts=4 sw=4 expandtab:
-# pylint: disable=no-self-use,protected-access
+
+import os
+from tempfile import TemporaryDirectory
 from unittest.mock import MagicMock, patch
 
+from hcoopmeetbotlogic.location import Location, Locations
 from hcoopmeetbotlogic.writer import write_meeting
+
+EXPECTED_LOG = os.path.join(os.path.dirname(__file__), "fixtures/test_writer/log.expected")
+EXPECTED_MINUTES = os.path.join(os.path.dirname(__file__), "fixtures/test_writer/minutes.expected")
+
+
+def _contents(path: str) -> str:
+    """Get contents of a file for comparison."""
+    with open(path, "r") as out:
+        return out.read()
 
 
 class TestFunctions:
     @patch("hcoopmeetbotlogic.writer.derive_locations")
     def test_write_meeting(self, derive_locations):
-        # TODO: expand this after implementing the function
-        locations = MagicMock()
-        derive_locations.return_value = locations
-        config = MagicMock()
-        meeting = MagicMock()
-        assert write_meeting(config, meeting) is locations
-        derive_locations.assert_called_once_with(config, meeting)
+        with TemporaryDirectory() as temp:
+            log = Location(path=os.path.join(temp, "log.expected"), url="http://")
+            minutes = Location(path=os.path.join(temp, "minutes.expected"), url="http://")
+            locations = Locations(log=log, minutes=minutes)
+            derive_locations.return_value = locations
+            config = MagicMock()
+            meeting = MagicMock()
+            assert write_meeting(config, meeting) is locations
+            derive_locations.assert_called_once_with(config, meeting)
+            assert _contents(log.path) == _contents(EXPECTED_LOG)
+            assert _contents(minutes.path) == _contents(EXPECTED_MINUTES)
