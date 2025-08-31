988ccae1d87c138c7663a83425e6e9a7deeb6062
Kenneth J. Pronovici <pronovic@ieee.org>
2025-08-31 18:30:44 -0500

    Address UP015

--- src/tests/hcoopmeetbotlogic/testdata.py
+++ src/tests/hcoopmeetbotlogic/testdata.py
@@ -11,7 +11,7 @@ START_TIME = datetime(2021, 4, 13, 2, 6, 12)
 
 def contents(path: str) -> str:
     """Get contents of a file for comparison."""
-    with open(path, "r", encoding="utf-8") as out:
+    with open(path, encoding="utf-8") as out:
         return out.read()
 
 
