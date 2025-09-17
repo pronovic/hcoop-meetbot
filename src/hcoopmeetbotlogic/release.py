af0795b82be17334dcd08d71e6354093503c72fb
Kenneth J. Pronovici <pronovic@ieee.org>
2025-09-17 18:12:23 -0500

    Refactor to pull release info from Python metadata

--- src/hcoopmeetbotlogic/release.py
+++ src/hcoopmeetbotlogic/release.py
@@ -4,8 +4,45 @@
 Version and release information.
 """
 
-AUTHOR = "Kenneth J. Pronovici"
-EMAIL = "pronovic@ieee.org"
-VERSION = "0.6.2"
-URL = "https://github.com/pronovic/hcoop-meetbot"
-DOCS = "https://hcoop-meetbot.readthedocs.io/en/stable/"
+# Historically, this information was tracked directly within this file as part of the
+# release process.  In modern Python, it's better to rely on the package metadata, which
+# is managed by Poetry on our behalf.
+#
+# The metadata will always be set any time the package has been completely and properly
+# installed, but defaults are provided for times when the package is not really installed.
+#
+# Note: previously, we also tracked release date and copyright date range, but that
+# information is not available in the package metadata.  These values are maintained to
+# avoid breaking the public interface, but are always "unset".
+
+from importlib.metadata import PackageMetadata, metadata
+from typing import cast
+
+try:
+    _METADATA = metadata("hcoop-meetbot")
+except ImportError:
+    _METADATA = cast("PackageMetadata", {})
+
+
+def _metadata(key: str, default: str) -> str:
+    return _METADATA[key] if key in _METADATA else default  # noqa
+
+
+def _project_url(key: str, default: str) -> str:
+    urls = _METADATA.get_all("Project-URL")
+    if urls is None:
+        urls = []
+    return next(
+        iter([url.replace(f"{key}, ", "") for url in urls if url.startswith(f"{key}, ")]),
+        default,
+    )
+
+
+AUTHOR = _metadata("Author", "unset")
+EMAIL = _metadata("Author-email", "unset")
+VERSION = _metadata("Version", "0.0.0")
+URL = _project_url("Homepage", "unset")
+DOCS = _project_url("Documentation", "unset")
+
+COPYRIGHT = "unset"
+DATE = "unset"
